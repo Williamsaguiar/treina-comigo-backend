9e3e0f9cae8a004343a6eabbf2128b49ae0a0f9c
Will <williamsaguiarjr@gmail.com>
2026-05-26 11:41:06 -0300

    neon database

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/treina_comigo"
+DATABASE_URL = "postgresql://neondb_owner:npg_Rpvj3k1LCFDu@ep-wispy-meadow-ac191iug.sa-east-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
