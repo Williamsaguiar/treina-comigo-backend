d29016b3ad35681c07166c3d187cdb73a9e9bb62
Will <williamsaguiarjr@gmail.com>
2026-05-28 09:08:30 -0300

    fix model

--- models.py
+++ models.py
@@ -1,6 +1,35 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
+
+class Usuario(Base):
+
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    nome = Column(String)
+
+    email = Column(String, unique=True)
+
+    senha = Column(String)
+
+
+class Treino(Base):
+
+    __tablename__ = "treinos"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    nome = Column(String)
+
+    objetivo = Column(String)
+
+    nivel = Column(String)
+
+    descricao = Column(String)
+
+
 class Academia(Base):
 
     __tablename__ = "academias"
